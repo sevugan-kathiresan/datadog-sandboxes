97848afac453019552467a490f8c517b87a9e6ee
sevugan-kathiresan <sevugan.kathiresan@datadoghq.com>
2026-01-19 15:02:20 +1100

    entrypoint.sh file created the startup shell script file

---
+++ python-docker/agri-api/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -e
+
+echo "Initializing database..."
+uv run python agri_api/init_db.py
+
+echo "Starting FastAPI..."
+exec uv run uvicorn agri_api.main:app --host 0.0.0.0 --port 8000
