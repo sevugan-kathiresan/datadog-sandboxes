e8d4e5f8be57f15f657ea8c0e146b5ab1d0511fe
sevugan-kathiresan <sevugan.kathiresan@datadoghq.com>
2026-01-20 15:37:32 +1100

    ddtrace installed and ddtrace-run command added to my applications startup command

--- python-docker/agri-api/entrypoint.sh
+++ python-docker/agri-api/entrypoint.sh
@@ -5,4 +5,4 @@ echo "Initializing database..."
 uv run python agri_api/init_db.py
 
 echo "Starting FastAPI..."
-exec uv run uvicorn agri_api.main:app --host 0.0.0.0 --port 8000
+exec uv run ddtrace-run uvicorn agri_api.main:app --host 0.0.0.0 --port 8000
